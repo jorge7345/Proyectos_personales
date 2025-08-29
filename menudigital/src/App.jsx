ba87bba37ca7073518feaf80c386cb569c55bd31
morenoaj <jorge.abuslaiman@gmail.com>
2025-08-29 12:10:20 -0300

    cambios en home

--- menudigital/src/App.jsx
+++ menudigital/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { HomePage } from './pages/homePage'
 
 function App() {
 
   return (
     <>
-      
+      <HomePage/>
     </>
   )
 }
--- menudigital/src/pages/homePage.jsx
+++ menudigital/src/pages/homePage.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { HeaderComponent } from '../components/headerComponent'
 
 export const HomePage = () => {
   return (
-    <div>homePage</div>
+    <div>
+      <HeaderComponent/>
+    </div>
   )
 }
